5adf3e7da108f491b1e006a434fc977234289aee
baybars <cem.baybars@gmail.com>
2019-11-25 15:57:12 +0300

    :fire: Remove unused import statements.

--- lib/localization/localizations.dart
+++ lib/localization/localizations.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show SynchronousFuture;
 import 'package:flutter/services.dart' show rootBundle;
 
 class AppLocalizations {
